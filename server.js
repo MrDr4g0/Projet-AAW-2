eaa52d694004f8b6d6d6718b7a0d2bc560c0598c
MrDr4g0 <102426043+MrDr4g0@users.noreply.github.com>
2023-01-19 16:21:21 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,118 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 3000;
+
+const animalTab =[
+    {
+        id: 1,
+        name: "Lion",
+        image: "/images/lion2.png",
+        notes: "Big cat",
+        favorite: true,
+    },
+    {
+        id: 2,
+        name: "Gorilla",
+        image: "/images/gorilla.png",
+        notes: "Big monkey",
+        favorite: false,
+    },
+    {
+        id: 3,
+        name: "Panda",
+        image: "/images/panda2.png",
+        notes: "Big chinese bear",
+        favorite: false,
+    },
+    {
+        id: 4,
+        name: "Crocodile",
+        image: "/images/croco2.png",
+        notes: "Big lizard",
+        favorite: true,
+    },
+    {
+        id: 5,
+        name: "Turtle",
+        image: "/images/turtle.png",
+        notes: "Sturdy lizard",
+        favorite: true,
+    },
+    {
+        id: 6,
+        name: "Zebra",
+        image: "/images/zebra.png",
+        notes: "Dual Color horse",
+        favorite: false,
+    },
+    {
+        id: 7,
+        name: "Giraffe",
+        image: "/images/giraffe.png",
+        notes: "Long neck horse",
+        favorite: true,
+    },
+    {
+        id: 8,
+        name: "Rhinoceros",
+        image: "/images/rhino.png",
+        notes: "it's grey and big",
+        favorite: false,
+    },
+
+    {
+        id: 9,
+        name: "Wolf",
+        image: "/images/wolf.png",
+        notes: "wild dog",
+        favorite: false,
+    },
+    {
+        id: 10,
+        name: "Dolphin",
+        image: "/images/dolphin.png",
+        notes: "smart fish",
+        favorite: true,
+    },
+
+];
+
+app.use(express.json()); // faut utiliser du json
+
+function generateNewId(){
+    return Math.random().toString(36).substring(2, 9);
+}
+
+
+app.get('/message',(req,res) => {
+    res.send('Mon premier Serveur NodeJs !')
+});
+
+app.get("/api/animals", (req, res)=>{
+    res.send(animalTab)
+});
+
+app.get("/api/animals:id", (req, res)=>{
+    res.send(animalTab.id)
+});
+
+
+app.post("/api/animals", (req, res)=>{
+    const animal = req.body;
+    console.log(animal)
+    animal.id = generateNewId(); // cette methode est à définir
+    animalTab.push(animal); // j'ajoute dans mon context le nouvel événement
+    res.send(animalTab)
+});
+
+
+
+app.use('/',express.static('public'));
+app.get('/*',(rec, res) =>{
+    res.sendFile(path.resolve("./public/index.html"))
+});
+
+app.listen(port, () => {
+    console.log(`Exemple app listening on port ${port}`);
+});
---
+++ src/route/listAnimal.jsx
@@ -0,0 +1,95 @@
+
+import { Outlet , Link, useLoaderData, Form,useSubmit, } from "react-router-dom";
+import { getAnimals, createAnimal } from "../app/animals"; // fichier animal
+import { useEffect} from "react";
+
+export async function loader({request}) {
+
+
+    const url = new URL(request.url);
+    const q = url.searchParams.get("q"); // prend le mot dans la barre de recherche
+
+    const animals = await getAnimals(q); //recherche tout les animaux
+
+    return { animals , q };
+}
+
+export async function action() {
+    await createAnimal();
+}
+
+export default function ListAnimal() {
+    const { animals , q } = useLoaderData();
+    useEffect(() => {
+        document.getElementById("q").value = q;
+        //animals.getElementsByName("q").value = q;
+    }, [q]);
+    const submit = useSubmit();
+    return (
+        <>
+            <div id="sidebar">
+                <h1>Liste des Animaux :</h1>
+                <nav>
+                    {animals.length ? (
+                        <ul>
+                            {animals.map((animal) => (
+                                <li key={animal.id}>
+                                    <Link to={`animals/${animal.id}`}>
+                                        {animal.name ? (
+                                            <>
+                                                {animal.name}
+                                            </>
+                                        ) : (
+                                            <i>No Name</i>
+                                        )}{" "}
+                                        {animal.favorite && <span>★</span>}
+                                    </Link>
+                                </li>
+                            ))}
+                        </ul>
+                    ) : (
+                        <p>
+                            <i>No Animals</i>
+                        </p>
+                    )}
+                </nav>
+                <div>
+                    <Form id="search-form" role="search">
+                        <input
+                            id="q"
+                            aria-label="Search animals"
+                            placeholder="Search"
+                            type="search"
+                            name="q"
+                            defaultValue={q}
+                            onChange={(event) => {
+                                const isFirstSearch = q == null;
+                                submit(event.currentTarget.form,{
+                                    replace: !isFirstSearch,
+                                });
+                            }}
+                        />
+                        <div
+                            id="search-spinner"
+                            aria-hidden
+                            hidden={true}
+                        />
+                        <div
+                            className="sr-only"
+                            aria-live="polite"
+                        ></div>
+                    </Form>
+                    <Form method="post">
+                        <button type="submit">New</button>
+                    </Form>
+                </div>
+
+
+
+            </div>
+            <div id="detail">
+                <Outlet />
+            </div>
+        </>
+    );
+}
